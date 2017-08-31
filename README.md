c4384053e7a24813e1efee74ff66040cfe93ec94
Tino Thamjarat <vtno@Tinos-MBP.localdomain>
2017-08-31 12:36:28 +0700

    Init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Bitdoi - บิทดอย
+
+Get latest prices and send a notification  to your [LINE](https://line.me/th/) account/group by [IFTTT webhook service](https://ifttt.com/maker_webhooks).
+
+You can deploy this to any web service with CRON job to get routine notifications.
+
+มาดอยกันเถอะ~
+
+# Installation
+
+You need Ruby `2.4.1` with `bundler`
+
+To install bundler run
+
+```
+gem install bundler
+```
+
+Then clone this project and run
+
+```
+bundle install
+```
+You need to put an `application.yml` file into your config file.
---
+++ fetch_coins_data.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'whenever'
+require 'net/http'
+require 'json'
+
+def fetch_coins_data
+  uri = URI('https://bx.in.th/api/')
+  res = Net::HTTP.get_response uri
+  data = JSON.parse res.body
+  btc = data['1']
+  eth = data['21']
+  xrp = data['25']
+  omg = data['26']
+
+  value1 = <<-TEXT
+  From https://bx.in.th/
+  BTC: ฿#{btc['last_price']}
+  ETH: ฿#{eth['last_price']}
+  XRP: ฿#{xrp['last_price']} (FOR PUSSY @min)
+  OMG: ฿#{omg['last_price']}
+  TEXT
+
+  req_objs = {
+    value1: value1
+  }
+
+  target_uri = URI("https://maker.ifttt.com/trigger/CRYPTO_SHIT/with/key/#{ENV['SECRET_KEY']}")
+  req = Net::HTTP.post_form(target_uri, req_objs)
+  puts req.body
+end
