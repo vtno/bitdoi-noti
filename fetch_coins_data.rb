7d10b0e7ae6f0ea9958ba91802674444b0f3cafb
consti <consti@consti.de>
2017-08-31 14:56:00 +0700

    remove bundler, dotenv dependencies

--- fetch_coins_data.rb
+++ fetch_coins_data.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
-require 'whenever'
 require 'net/http'
 require 'json'
-require 'dotenv/load'
 
-def fetch_coins_data
-  uri = URI('https://bx.in.th/api/')
-  res = Net::HTTP.get_response uri
-  data = JSON.parse res.body
-  btc = data['1']
-  eth = data['21']
-  xrp = data['25']
-  omg = data['26']
+IFTTT_KEY = ENV['IFTTT_KEY']
 
-  value1 = <<-TEXT
-  From https://bx.in.th/
-  BTC: ฿#{btc['last_price']}
-  ETH: ฿#{eth['last_price']}
-  XRP: ฿#{xrp['last_price']} (FOR PUSSY @min)
-  OMG: ฿#{omg['last_price']}
-  TEXT
+uri = URI('https://bx.in.th/api/')
+res = Net::HTTP.get_response uri
+data = JSON.parse res.body
+btc = data['1']
+eth = data['21']
+xrp = data['25']
+omg = data['26']
 
-  req_objs = {
-    value1: value1
-  }
-  target_uri = URI("https://maker.ifttt.com/trigger/CRYPTO_SHIT/with/key/#{ENV["SECRET_KEY"]}")
-  req = Net::HTTP.post_form(target_uri, req_objs)
-  puts req.body
-end
+value1 = <<-TEXT
+From https://bx.in.th/
+BTC: ฿#{btc['last_price']}
+ETH: ฿#{eth['last_price']}
+XRP: ฿#{xrp['last_price']}
+OMG: ฿#{omg['last_price']}
+TEXT
 
-fetch_coins_data
+req_objs = { value1: value1 }
+target_uri = URI(
+  "https://maker.ifttt.com/trigger/CRYPTO_SHIT/with/key/#{IFTTT_KEY}"
+)
+req = Net::HTTP.post_form(target_uri, req_objs)
+puts req.body
