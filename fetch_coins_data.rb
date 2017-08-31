4b52f6ec2bda9857b5b4e50f4f1d03f601c04222
Tino Thamjarat <vtno@Tinos-MBP.localdomain>
2017-08-31 15:12:51 +0700

    Remove unnecessary gem

--- fetch_coins_data.rb
+++ fetch_coins_data.rb
@@ -1,33 +1,23 @@
 # frozen_string_literal: true
 
-require 'whenever'
 require 'net/http'
 require 'json'
-require 'dotenv/load'
 
-def fetch_coins_data
-  uri = URI('https://bx.in.th/api/')
-  res = Net::HTTP.get_response uri
-  data = JSON.parse res.body
-  btc = data['1']
-  eth = data['21']
-  xrp = data['25']
-  omg = data['26']
+res = Net::HTTP.get_response URI('https://bx.in.th/api/')
+data = JSON.parse res.body
+btc = data['1']
+eth = data['21']
+xrp = data['25']
+omg = data['26']
 
-  value1 = <<-TEXT
+value1 = <<-TEXT
   From https://bx.in.th/
   BTC: ฿#{btc['last_price']}
   ETH: ฿#{eth['last_price']}
-  XRP: ฿#{xrp['last_price']} (FOR PUSSY @min)
+  XRP: ฿#{xrp['last_price']}
   OMG: ฿#{omg['last_price']}
-  TEXT
+TEXT
 
-  req_objs = {
-    value1: value1
-  }
-  target_uri = URI("https://maker.ifttt.com/trigger/CRYPTO_SHIT/with/key/#{ENV["SECRET_KEY"]}")
-  req = Net::HTTP.post_form(target_uri, req_objs)
-  puts req.body
-end
-
-fetch_coins_data
+target_uri = URI("https://maker.ifttt.com/trigger/CRYPTO_SHIT/with/key/#{ENV['IFTTT_KEY']}")
+req = Net::HTTP.post_form(target_uri, value1: value1)
+puts req.body
