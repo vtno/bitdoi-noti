be977c29750f8b357bb5aada933ecd56ee1e77d8
Tino Thamjarat <tino.thamjarat@gmail.com>
2017-09-06 12:06:04 +0700

    Can input sleep time.

--- fetch_bx_data.rb
+++ fetch_bx_data.rb
@@ -12,13 +12,14 @@ eth = data['21']
 xrp = data['25']
 omg = data['26']
 
-puts data
-
 value1 = <<-TEXT
   OMG: ฿#{omg['last_price']} ETH: ฿#{eth['last_price']} XRP: ฿#{xrp['last_price']} BTC: ฿#{btc['last_price']}
   From bx
 TEXT
 
 target_uri = URI("https://maker.ifttt.com/trigger/CRYPTO_NOTI/with/key/#{IFTTT_KEY}")
+target_uri2 = URI("https://maker.ifttt.com/trigger/CRYPTO_NOTI_OOZOU/with/key/#{IFTTT_KEY}")
 req = Net::HTTP.post_form(target_uri, value1: value1)
+req2 = Net::HTTP.post_form(target_uri2, value1: value1)
 puts req.body
+puts req2.body
--- iterate_fetch.rb
+++ iterate_fetch.rb
@@ -1,5 +1,5 @@
 loop do
   `IFTTT_KEY=#{ENV['IFTTT_KEY']} ruby fetch_bx_data.rb`
   `IFTTT_KEY=#{ENV['IFTTT_KEY']} ruby fetch_finex_data.rb`
-  sleep 60
+  sleep ENV['SLEEP'].to_i
 end
